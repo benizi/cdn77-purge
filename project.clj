f94d5d806401f113a8eaa4305a6a29fbd2badaff
Mattias W <mattiasw@gmail.com>
2015-09-20 10:34:37 +0200

    Using lein ancient, I found some old versions. Update them. Experiment with (refresh) that loads all edited files

--- project.clj
+++ project.clj
@@ -1,12 +1,18 @@
+;;; TIP: lein ancient to find updates
+
 (defproject cdn77purge "0.1.0-SNAPSHOT"
   :description "Loop over all pages on your site and prefetch the changed ones"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.1.19"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 ;; (refresh) seems to work better if I switch to user first: (in-ns 'user)
+                 ;; user=> (require '[clojure.tools.namespace.repl :refer [refresh]])
+                 ;; user=> (refresh)
+                 [org.clojure/tools.namespace "0.2.11"]
                  ]
   :main ^:skip-aot cdn77purge.core
   :target-path "target/%s"
